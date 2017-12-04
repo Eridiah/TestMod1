2ebaa51a80e40fb73d3ed61893a4f5c8ffdabf12
EridiahMods <eridiahmods@gmail.com>
2017-12-03 22:44:05 -0500

    Added Tin, Copper, and Steel Rings
    All new items have textures. Also added Copper Ingot and Steel Ingot
    with crafting recipes for all rings. Also slightly tweaked the Tin
    Ingot texture.

--- src/main/java/com/eridiah/testmod1/common/init/InitItems.java
+++ src/main/java/com/eridiah/testmod1/common/init/InitItems.java
@@ -12,7 +12,11 @@ public class InitItems
 {
 	// Other Stuff
 	public static Item broken_blade;
+	
+	// Minerals
 	public static Item tin_ingot;
+	public static Item copper_ingot;
+	public static Item steel_ingot;
 	
 	// Chain Mail Stuff
 	public static Item iron_ring, gold_ring, diamond_ring, emerald_ring; // Default Ore Rings
@@ -25,12 +29,18 @@ public class InitItems
 	public static void init() 
 	{
 		broken_blade = new ItemBasic("broken_blade");
+		
 		tin_ingot = new ItemBasic("tin_ingot");
+		copper_ingot = new ItemBasic("copper_ingot");
+		steel_ingot = new ItemBasic("steel_ingot");
 		
 		iron_ring = new ItemBasic("iron_ring");
 		gold_ring = new ItemBasic("gold_ring");
 		diamond_ring = new ItemBasic("diamond_ring");
 		emerald_ring = new ItemBasic("emerald_ring");
+		tin_ring = new ItemBasic("tin_ring");
+		copper_ring = new ItemBasic("copper_ring");
+		steel_ring = new ItemBasic("steel_ring");
 		
 		chain_mail = new ItemBasic("chain_mail");
 		
@@ -40,12 +50,18 @@ public class InitItems
 	public static void register()
 	{
 		registerItem(broken_blade);
+		
 		registerItem(tin_ingot);
+		registerItem(copper_ingot);
+		registerItem(steel_ingot);
 		
 		registerItem(iron_ring);
 		registerItem(gold_ring);
 		registerItem(diamond_ring);
 		registerItem(emerald_ring);
+		registerItem(tin_ring);
+		registerItem(copper_ring);
+		registerItem(steel_ring);
 		
 		registerItem(chain_mail);
 		
